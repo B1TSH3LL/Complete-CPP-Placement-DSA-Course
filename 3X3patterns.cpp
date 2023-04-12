45dbe2d18f95c59bbd933ca8fbf7c550d35aa45f
CyberBreach <shrivastavapiyush781@gmail.com>
2023-04-12 11:00:44 +0530

    new patterns

---
+++ 3X3patterns.cpp
@@ -0,0 +1,30 @@
+// * * *
+// * * *
+// * * *
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    int n;
+    int i = 1;
+    cout << "Enter the number of stars to display : ";
+    cin >> n;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "* ";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
+    return 0;
+}
