6592c50b051cb09fe095e6721600e201946df721
CyberBreach <shrivastavapiyush781@gmail.com>
2023-04-17 22:12:26 +0530

    C++ Placement DSA Course Follow-Up

--- LeetCode/problem1.cpp
+++ LeetCode/problem1.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+// 1281. Subtract the Product and Sum of Digits of an Integer
 
-using namespace std;
-
-int main()
+class Solution
 {
+public:
+    int subtractProductAndSum(int n)
+    {
+        int product = 1;
+        int sum = 0;
 
-    return 0;
-}
+        while (n != 0)
+        {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+
+            n /= 10;
+        }
+
+        int ans = product - sum;
+
+        return ans;
+    }
+};
